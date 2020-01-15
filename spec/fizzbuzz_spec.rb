1ac9f13c31ec4d2ba54011622c4e6f5d8924765c
EC2 Default User <ec2-user@ip-172-31-45-41.eu-west-2.compute.internal>
2020-01-15 17:26:15 +0000

    fizzbuzz

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,18 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+    it 'returns "fizz" when passed 3' do
+        expect(3.fizzbuzz).to eq 'fizz'
+    end
+    it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+    end
+    it 'returns "4" when passed 4' do
+    expect(4.fizzbuzz).to eq 4
+    end
+    it 'returns "fizzbuzz" when passed 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+    end
+end
+
+
